6fe23c7e6fae0e30aa321e3ec877b0bfa127ae1b
Yegor <egoryefim@gmail.com>
2020-11-05 16:08:46 +0200

    сервис для работы с API

---
+++ src/js/apiService.js
@@ -0,0 +1,45 @@
+const API_KEY = '18976162-4407e31cd80a0810b100a4c9f';
+const BASE_URL = 'https://pixabay.com/api/';
+
+
+
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImages() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    // return fetch(url)
+    //   .then(response => response.json())
+    //     .then((data) => {
+    //         this.incrementPage();
+    //       return data.hits
+        
+        
+    //   });
+
+    const response = await fetch(url);
+    const newImage = await response.json();
+    this.incrementPage();
+    return newImage.hits;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
